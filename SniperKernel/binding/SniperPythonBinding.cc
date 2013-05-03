d08dcc494aac15b234bd4875eb740a105b512c47
lint <lintao@ihep.ac.cn>
2013-05-03 17:34:20 +0800

    Add singleton into python.

---
+++ SniperKernel/binding/SniperPythonBinding.cc
@@ -0,0 +1,15 @@
+
+#include "boost/noncopyable.hpp"
+#include "boost/python.hpp"
+using namespace boost::python;
+
+#include "SniperKernel/OptionParser.h"
+
+BOOST_PYTHON_MODULE(libSniperPython)
+{
+    class_<OptionParser, boost::noncopyable>("OptionParser", no_init)
+        .def("OP", &OptionParser::instance, 
+                return_value_policy<reference_existing_object>())
+        .staticmethod("OP")
+    ;
+}
