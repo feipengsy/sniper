b3967fcb5a37cc63bf231b4ca0d2cdfce4695ba8
lint <lintao@ihep.ac.cn>
2013-05-03 20:15:14 +0800

    Add execute.

--- SniperKernel/binding/SniperPythonBinding.cc
+++ SniperKernel/binding/SniperPythonBinding.cc
@@ -42,6 +42,7 @@ BOOST_PYTHON_MODULE(libSniperPython)
         .def("instance", &AlgMgr::instance,
                 return_value_policy<reference_existing_object>())
         .staticmethod("instance")
+        .def("execute", &AlgMgr::execute)
 
     ;
 
