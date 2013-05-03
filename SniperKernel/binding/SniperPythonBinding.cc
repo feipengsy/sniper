6b920e3454e8300b56e9ff7cbb4fbbc6a3987207
lint <lintao@ihep.ac.cn>
2013-05-03 19:06:49 +0800

    add the static method. but it seems not work correctly.

--- SniperKernel/binding/SniperPythonBinding.cc
+++ SniperKernel/binding/SniperPythonBinding.cc
@@ -11,5 +11,9 @@ BOOST_PYTHON_MODULE(libSniperPython)
         .def("OP", &OptionParser::instance, 
                 return_value_policy<reference_existing_object>())
         .staticmethod("OP")
+        .def("addOption", &OptionParser::addOption)
+        .staticmethod("addOption")
+        .def("setOption", &OptionParser::setOption<int>)
+        .staticmethod("setOption")
     ;
 }
