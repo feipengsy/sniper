758337c307872aa9eb462b2df5f06afe7c7250e3
lint <lintao@ihep.ac.cn>
2013-07-17 09:46:56 +0800

    rename the user buffer mgr contents name. but still need to configure in other method.

--- SniperKernel/src/UserBufMgr.cc
+++ SniperKernel/src/UserBufMgr.cc
@@ -1,32 +1,14 @@
 #include "SniperKernel/UserBufMgr.h"
 #include "SniperKernel/SniperLog.h"
+#include "SniperKernel/property.hh"
 
 UserBufMgr* UserBufMgr::_instance = 0;
 
 template<> ElementMgr<UserBuffer>::ElementMgr(const std::string& name)
     : m_name(name)
 {
-    std::vector<std::string> vNames;
-    //OptionParser::setOption(name, "Selectors", vNames);
+    declareProperty(name, "Selectors", vNames);
 
-    for(std::vector<std::string>::iterator i=vNames.begin(); i!=vNames.end(); ++i) {
-	std::string typName = *i;
-	std::string objName = typName;
-	std::string::size_type pseg = typName.find('/', 0);
-	if ( pseg != std::string::npos ) {
-	    typName = typName.substr(0, pseg);
-	    objName = objName.substr(pseg+1, std::string::npos);
-	}
-
-	Type2CreatorMap::iterator j = elementCreatorMap.find(typName);
-	if ( j == elementCreatorMap.end() ) {
-	    std::string msg = name + ": unknown selector " + typName;
-	    throw SniperException(msg);
-	}
-	ElementPtr obj ( (j->second)(objName) );
-	m_elements.push_back(obj);
-	name2obj[objName] = obj;
-    }
 }
 
 template<> bool ElementMgr<UserBuffer>::initialize()
